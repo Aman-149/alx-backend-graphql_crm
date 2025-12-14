a310d43632ea08a6174a29bb855a8a519198bdf1
Aman-149 <amanuelfikadu23971@gmail.com>
2025-12-14 23:53:16 +0300

    Add CustomerFilter for filtering customers

--- crm/filters.py
+++ crm/filters.py
@@ -43,3 +43,10 @@ class OrderFilter(django_filters.FilterSet):
             'order_date': ['gte', 'lte'],
         }
 
+import django_filters
+from .models import Customer
+
+class CustomerFilter(django_filters.FilterSet):
+    class Meta:
+        model = Customer
+        fields = ['name', 'email']
