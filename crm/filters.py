3f5a50ffc3f17d5b8c2e4bb26cf7fd88120589a6
Aman-149 <amanuelfikadu23971@gmail.com>
2025-12-14 23:56:22 +0300

    Update filters.py

--- crm/filters.py
+++ crm/filters.py
@@ -94,3 +94,11 @@ class CustomerNode(DjangoObjectType):
 # ---------------- Query ----------------
 class Query(graphene.ObjectType):
     all_customers = DjangoFilterConnectionField(CustomerNode)
+
+import django_filters
+from .models import Customer
+
+class CustomerFilter(django_filters.FilterSet):
+    class Meta:
+        model = Customer
+        fields = ['name', 'email']
